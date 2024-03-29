a0377d00475f17343ae1a63b28ccbe6260034ae1
matthew-pisano <matthewpisano14@gmail.com>
2024-03-29 16:09:48 -0400

    Reformat and Document

--- data/README.md
+++ data/README.md
@@ -1,3 +1,3 @@
 # Data Folder
 
-This is the folder where prompt datasets, model responses and evaluations will be stored.
+This is the folder where prompt datasets, model responses and evaluations will be stored.  If not already provided (e.g. during the review process), the data can be made available upon a request to the authors.
--- src/constants.py
+++ src/constants.py
@@ -8,5 +8,6 @@ console_handler.setLevel(logging.DEBUG)
 console_handler.setFormatter(logging.Formatter('[%(levelname)s %(name)s @ %(asctime)s] %(message)s', "%H:%M:%S"))
 logger.addHandler(console_handler)
 
+# Filter out some noisy logs
 logging.getLogger("httpx").setLevel(logging.WARNING)
 logging.getLogger("openai._base_client").setLevel(logging.WARNING)
